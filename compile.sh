f8d14080030426e4f30229d44e8cd592e1df6e5b
Marcello Seri <marcello.seri@gmail.com>
2024-06-16 19:33:55 +0200

    Update generation script
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash
 
-pdflatex --interaction=nonstopmode hm.tex
-pdflatex --interaction=nonstopmode hm.tex
+lualatex --interaction=nonstopmode hm.tex
+lualatex --interaction=nonstopmode hm.tex
 bibtex hm
-pdflatex --interaction=nonstopmode hm.tex
+lualatex --interaction=nonstopmode hm.tex
 lwarpmk html
 lwarpmk limages
 bibtex hm_html
