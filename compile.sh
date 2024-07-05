2ebb1da913907821ff2a5e65ccae8306951d5eea
Marcello Seri <marcello.seri@gmail.com>
2024-07-05 21:47:48 +0200

    Update compilation script
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
 
 lualatex --interaction=nonstopmode hm.tex
-lualatex --interaction=nonstopmode hm.tex
 bibtex hm
 lualatex --interaction=nonstopmode hm.tex
-lwarpmk html
+lualatex --interaction=nonstopmode hm.tex
 lwarpmk limages
-bibtex hm_html
 lwarpmk html
+bibtex hm_html
+lwarpmk html1
