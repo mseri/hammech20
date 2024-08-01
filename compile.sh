ebde653b6811165a0ad143535b9a454200b0db45
Marcello Seri <marcello.seri@gmail.com>
2024-08-01 17:43:13 +0200

    Update
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- compile.sh
+++ compile.sh
@@ -2,9 +2,11 @@
 
 lualatex --interaction=nonstopmode hm.tex
 bibtex hm
+makeindex hm
 lualatex --interaction=nonstopmode hm.tex
 lualatex --interaction=nonstopmode hm.tex
 lwarpmk limages
 lwarpmk html
 bibtex hm_html
+lwarpmk printindex
 lwarpmk html1
