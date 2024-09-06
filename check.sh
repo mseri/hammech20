d1225a75e02be41229a7f3e9260699fdde637711
Marcello Seri <marcello.seri@gmail.com>
2024-09-06 10:55:38 +0200

    new attempt
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- check.sh
+++ check.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+cat << EOF > /etc/fonts/conf.d/09-texlive.conf
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+  <dir>/usr/share/texmf-dist/fonts/opentype</dir>
+</fontconfig>
+EOF
 
 fc-cache
 fc-list :outline -f "%{family}\n" | grep -i Open
