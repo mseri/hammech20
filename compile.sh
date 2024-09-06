dabb7d3ffa7c730f63717945e36dec430db2ffc6
Marcello Seri <mseri@users.noreply.github.com>
2024-09-06 17:28:32 +0200

    Fix build (#3)
    * Another attempt
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Fix typo
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Revert hm changes
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Apply the font with a patch
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * last attempt for tonight
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * new attempt
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * try something else
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Fix
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update using apt
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Add missing texlive package and cache apt install
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update gitignore
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Add missing apt package
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update patchfiles
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update cation
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Install bibtex
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * getting there
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Small cleanup
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    * Update
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>
    
    ---------
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- compile.sh
+++ compile.sh
@@ -1,15 +1,12 @@
 #!/bin/env bash
 
-if [ -n "$CI" ] || [ -n "$GITHUB_RUN_ID" ]; then
-  sudo rm -rf /usr/share/fonts/woff/opendyslexic
-fi
 lualatex --interaction=nonstopmode hm.tex
 bibtex hm
 makeindex hm
 lualatex --interaction=nonstopmode hm.tex
 lualatex --interaction=nonstopmode hm.tex
-lwarpmk limages
 lwarpmk html
+lwarpmk limages
 bibtex hm_html
 lwarpmk printindex
 lwarpmk html1
