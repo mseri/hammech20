33db935dd9483a4a462006f1dd4c9410e0da20c5
Marcello Seri <marcello.seri@gmail.com>
2024-09-06 11:14:31 +0200

    try something else
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- check.sh
+++ check.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-cat << EOF > /etc/fonts/conf.d/09-texlive.conf
-<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-  <dir>/usr/share/texmf-dist/fonts/opentype</dir>
-</fontconfig>
-EOF
+# cat << EOF > /etc/fonts/conf.d/09-texlive.conf
+# <?xml version="1.0"?>
+# <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+# <fontconfig>
+#   <dir>/usr/share/texmf-dist/fonts/opentype</dir>
+# </fontconfig>
+# EOF
 
 fc-cache -vf
-fc-list :outline -f "%{family}\n" | grep -i Open
+fc-list :outline -f "%{family}\n" | grep -ie "Open|Gyre"
